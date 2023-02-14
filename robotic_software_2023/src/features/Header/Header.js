001f12f182ff854c296e83492c7033e4af5a432c
Jonathan Leger <jojox110@pop-os.localdomain>
2023-02-13 22:05:12 -0400

    sync

--- robotic_software_2023/src/features/Header/Header.js
+++ robotic_software_2023/src/features/Header/Header.js
@@ -1,49 +1,53 @@
-import React, { useState } from "react";
-import {
-  headerStyles,
-  headerStylesLinks,
-  headerStylesLogo,
-  menuDropdownStyling,
-} from "./HeaderStyling";
+import React from "react";
+
+import styles from '../../Styles/HeaderStyling.module.css'
+import {useNavigate} from "react-router-dom";
 
 // Import this component into every other component, do NOT render Header directly into App
 
 export function Header() {
-  // const handleChange = (type) => {
-  //   console.log('here')
-  //   if (type === "none") {
-  //     menuDropdownStyling.display = "none";
-  //   } else if (type === "block") {
-  //     menuDropdownStyling.display = "block";
-  //   }
-  // };
+    // const handleChange = (type) => {
+    //   console.log('here')
+    //   if (type === "none") {
+    //     menuDropdownStyling.display = "none";
+    //   } else if (type === "block") {
+    //     menuDropdownStyling.display = "block";
+    //   }
+    // };
 
-  return (
-    <header style={headerStyles}>
-      <section style={headerStylesLogo}>COMPETITION LOGO</section>
-      <section style={headerStylesLinks}>
-        <p>Home</p>
-        <p>Shop</p>
-        <div>
-          <p>Docs</p>
-          <div
-            style={menuDropdownStyling}
-            // onMouseOver={handleChange("block")}
-            // onMouseOut={handleChange("none")}
-          >
-            <a href="#">Docs</a>
-            <a href="#">Examples</a>
-          </div>
-        </div>
-        <div>
-          <p>Scores</p>
-          <div style={menuDropdownStyling}>
-            <a href="#">Current</a>
-            <a href="#">All</a>
-          </div>
-        </div>
-        <p>Schedule</p>
-      </section>
-    </header>
-  );
+
+    let navigate = useNavigate()
+
+    const redirectToScoreCurrent = () => {
+        console.log('current')
+        return navigate('/score/current')
+    }
+    const redirectToScoreAll = () => {
+        return navigate('/score/all/')
+    }
+
+    return (<header className={styles.headerStyles}>
+        <section className={styles.headerStylesLogo}>COMPETITION LOGO</section>
+        <section className={styles.headerStylesLinks}>
+            <p>Home</p>
+            <p>Shop</p>
+            <div className={styles.dropdown}>
+                <p>Docs</p>
+                <div
+                    className={styles.menuDropdownStyling}
+                >
+                    <a href="#">Docs</a>
+                    <a href="#">Examples</a>
+                </div>
+            </div>
+            <div className={styles.dropdown}>
+                <p>Scores</p>
+                <div className={styles.menuDropdownStyling}>
+                    <button onClick={redirectToScoreCurrent}>Current</button>
+                    <button onClick={redirectToScoreAll}>All</button>
+                </div>
+            </div>
+            <p>Schedule</p>
+        </section>
+    </header>);
 }
