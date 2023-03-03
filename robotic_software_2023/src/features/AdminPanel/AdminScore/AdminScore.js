b5d0fd1639aea90bd385f191b58156f009b0b253
jojox110 <jonathansamuel.leger@gmail.com>
2023-03-03 09:46:51 -0400

    sync

--- robotic_software_2023/src/features/AdminPanel/AdminScore/AdminScore.js
+++ robotic_software_2023/src/features/AdminPanel/AdminScore/AdminScore.js
@@ -7,51 +7,11 @@ import {AdminTeamDisplayComponent} from "./AdminTeamDisplayComponent";
 import styles from '../../../Styles/AdminPanel.module.css'
 
 export function AdminScore() {
-<<<<<<< HEAD
-    return (<div className={styles.body}>
-            <Header/>
-            <article className={styles.scoreBoardGrid}>
-                <section className={styles.scores}>
-                    <AdminTeamDisplayComponent teamName={"team1"}/>
-                    <AdminTeamDisplayComponent teamName={"team2"}/>
-                    <AdminTeamDisplayComponent teamName={"team3"}/>
-                </section>
-            </article>
-        </div>);
-=======
-  //const dispatch = useDispatch();
-
-  const updateTeamDisplayScore = (teamID) => {};
-
-  const onTeamButtonClick = (event) => {
-    // if (teamDisplays.length === 3) {
-    //   alert("Already have 3 teams");
-    //   teamDisplays.length = 0;
-    //   return;
-    // }
-
-    //renderTeamDisplay(1);
-    console.log(event);
-  };
-
   return (
-    <div style={body}>
+    <div className={styles.body}>
       <Header />
-      <article style={scoreBoardGrid}>
-        {/* <section style={teams}>
-          <table>
-            <tbody>
-              <tr style={teamsTable}>
-                <td>
-                  <button style={teamsButton} value={1}>
-                    team1
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section> */}
-        <section style={scores}>
+      <article className={styles.scoreBoardGrid}>
+        <section className={styles.scores}>
           <AdminTeamDisplayComponent teamName={"team1"} />
           <AdminTeamDisplayComponent teamName={"team2"} />
           <AdminTeamDisplayComponent teamName={"team3"} />
@@ -59,5 +19,4 @@ export function AdminScore() {
       </article>
     </div>
   );
->>>>>>> 117dbe6 (sync)
 }
--- robotic_software_2023/src/features/AdminPanel/AdminScore/AdminTeamDisplayComponent.js
+++ robotic_software_2023/src/features/AdminPanel/AdminScore/AdminTeamDisplayComponent.js
@@ -1,14 +1,28 @@
 import React from  'react';
 import styles from '../../../Styles/AdminPanel.module.css'
 import ljr from '../../../SchoolLogos/Logo_LJR_brun_jaune.png'
+import mm from '../../../SchoolLogos/Logo_Mathieu-Martin.png'
 
 export function AdminTeamDisplayComponent(props) {
     return (
         <div className={styles.teamDisplayStyling}>
             <div className={styles.teamDisplayGap}>
-                 <button>
-                     <img src={ljr} alt='LJR logo' />
-                 </button>
+                 <div>
+                     <img src={mm} alt='MM logo' />
+                     <div>
+                        <button>Team 1</button>
+                        <button>Team 2</button>
+                        <button>Team 3</button>
+                        <button>Team 4</button>
+                        <button>Team 5</button>
+                        <button>Team 6</button>
+                        <button>Team 7</button>
+                        <button>Team 8</button>
+                        <button>Team 9</button>
+                        <button>Team 10</button>
+                        <button>Team 11</button>
+                     </div>
+                 </div>
                 <div>Amount of points</div>
                 <div className={styles.teamDisplayButtonsContainer}>
                     <button className={styles.teamDisplayButtons}>+</button>
